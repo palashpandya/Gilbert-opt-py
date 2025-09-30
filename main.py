99579ee62e4e309b85f4eb68bc7a38987c570df4
palashpandya <palashpandya775@gmail.com>
2025-09-30 11:15:41 +0200

    Added the use of the function 'generate-report'.

--- main.py
+++ main.py
@@ -4,18 +4,15 @@ from os import path
 import numpy as np
 # from numpy import ndarray, dtype, generic, bool_
 from scipy import io
-from functions import gilbert
+from functions import gilbert, generate_report
 import configparser
 
-# from prompt_toolkit.utils import to_str
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # rho = random_pure_dl([2, 2])
     # rho = make_density(rho)
     config = configparser.ConfigParser()
     config.read('config.ini')
-    inpFile = config['DEFAULT']['inputState'];
+    inpFile = config['DEFAULT']['inputState']
     outFile = config['DEFAULT']['outputState']
     outFile2 = config['DEFAULT']['outputStatistics']
     dim_list = np.fromstring(config['DEFAULT']['dimList'], dtype=int, sep=' ')
@@ -29,35 +26,6 @@ if __name__ == '__main__':
         print("ERROR: Unable to find the input matrix. Check the name supplied in the config.")
         exit(0)
 
-    rho1 = np.array([[0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 1 / 3., 1 / 3., 0, 1 / 3., 0, 0, 0],
-                    [0, 1 / 3., 1 / 3., 0, 1 / 3., 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 1 / 3., 1 / 3., 0, 1 / 3., 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0]]).astype(complex)
-    print((dim_list))
-    if rho.all()==rho1.all():
-        print("TRUE")
-    if dim_list2.all()==dim_list.all():
-        print("TRUE")
-
-    # rho = np.array([[0.5, 0, 0, 0, 0, 0, 0, 0.5],
-    #                 [0, 0., 0 / 3., 0, 0 / 3., 0, 0, 0],
-    #                 [0, 0 / 3., 0 / 3., 0, 0 / 3., 0, 0, 0],
-    #                 [0, 0, 0, 0, 0, 0, 0, 0],
-    #                 [0, 0 / 3., 0 / 3., 0, 0 / 3., 0, 0, 0],
-    #                 [0, 0, 0, 0, 0, 0, 0, 0],
-    #                 [0, 0, 0, 0, 0, 0, 0, 0],
-    #                 [0.5, 0, 0, 0, 0, 0, 0, 0.5]]).astype(complex)
-    # file_name = input('Enter the name for a matrix file (*.mtx): ')
-    # if os.path.exists(file_name):
-    #     rho = sp.io.mmread(file_name)
-    # else:
-    #     print("Unable to find the matrix.")
-    # print(rho)
-
     start = time.time()
     css, dist, trials, dist_list = gilbert(rho,  dim_list, max_iter, max_trials, opt_state="on")  # (, rho1_in=approx)
     stop = time.time()
@@ -65,5 +33,6 @@ if __name__ == '__main__':
     print(css, dist, trials, stop-start)
     io.mmwrite(outFile, css)
     np.savetxt(outFile2, dist_list)
+    generate_report(dist_list)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
