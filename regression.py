d67491dd643cd718a362e245696452de2dbed227
palashpandya <palashpandya775@gmail.com>
2025-10-12 22:44:31 +0200

    Changed the fitting function much better suited to the data.

---
+++ regression.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from scipy.optimize import curve_fit
+from matplotlib import pyplot as plt
+
+
+def fit_func(x,a,b,c):
+    return (b/x)**(c)+a
+
+dist = np.loadtxt('output_series.csv',delimiter=',',dtype=float)
+# Discard the first third
+red_dist = np.array([x for x in dist[int(len(dist)/3):]])
+#corrections
+X = np.array([ x+int(len(dist)/3) for x in range(len(red_dist))])
+popt, pcov = curve_fit(fit_func, X,red_dist)
+print(popt,pcov)
+pred = popt[0]
+plt.plot(X, [fit_func(x, *popt) for x in X], 'g-',
+          label='fit: a=%f' % pred)
+plt.scatter(X,red_dist,color='red')
+plt.show()
+
